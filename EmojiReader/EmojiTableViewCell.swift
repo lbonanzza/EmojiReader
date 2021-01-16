926c47d3b9300f212f0abca9c1eff0ce95670f25
Alexey Kolesnik <lbonanzza@gmail.com>
2021-01-16 20:12:03 +0300

    Add custom cell

---
+++ EmojiReader/EmojiTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiReader
+//
+//  Created by alexeykolesnik on 16.01.2021.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
