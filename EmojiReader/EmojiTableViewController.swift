0f3edcc4810a1430e6884b7f6bc34d4326646ea9
Alexey Kolesnik <lbonanzza@gmail.com>
2021-01-18 18:44:25 +0300

    added the "done" function by swiping to the left

--- EmojiReader/EmojiTableViewController.swift
+++ EmojiReader/EmojiTableViewController.swift
@@ -70,4 +70,22 @@ class EmojiTableViewController: UITableViewController {
     }
     
     
+    // add swipe button
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = doneAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [done])
+    }
+    
+    func doneAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.backgroundColor = .systemGreen
+        action.image = UIImage(systemName: "checkmark.circle")
+        return action
+    }
+    
+    
 }
