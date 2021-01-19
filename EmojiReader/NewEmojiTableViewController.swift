8eaa8760b5fa3d666ed762806b5d95cae3643ff9
Alexey Kolesnik <lbonanzza@gmail.com>
2021-01-19 18:48:20 +0300

    added the ability to edit a cell

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
 
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourit: false)
     
     @IBOutlet weak var emojiTF: UITextField!
     @IBOutlet weak var nameTF: UITextField!
@@ -17,10 +18,40 @@ class NewEmojiTableViewController: UITableViewController {
     
     
     @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButtonState()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
+        updateSaveButtonState()
+    }
+    
+    private func updateUI() {
+        emojiTF.text = emoji.emoji
+        nameTF.text = emoji.name
+        descriptionTF.text = emoji.description
+    }
+    
+    private func updateSaveButtonState() {
+        let emojiText = emojiTF.text ?? ""
+        let nameText = nameTF.text ?? ""
+        let descriptionText = descriptionTF.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard segue.identifier == "saveSegue" else { return }
+        
+        let emoji = emojiTF.text ?? ""
+        let name = nameTF.text ?? ""
+        let description = descriptionTF.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourit: self.emoji.isFavourit)
         
     }
 
